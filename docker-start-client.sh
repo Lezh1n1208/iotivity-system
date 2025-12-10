4fe25b971e05067bbe90171b4670ce0b8184e0e9
Lezh1n <hoangphilong1208@gmail.com>
2025-12-10 10:23:57 +0700

    clean code

--- docker-start-client.sh
+++ docker-start-client.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# filepath: /home/lezh1n/Workspace/Project/IoT/source-code/docker-start-client.sh
-
 echo "📱 Starting OCF Client"
 sleep 5
 exec /app/ocfclient
--- docker-start-server.sh
+++ docker-start-server.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# filepath: /home/lezh1n/Workspace/Project/IoT/source-code/docker-start-server.sh
-
 echo "🚀 Starting IoT Gateway"
 
 cd /app/backend && python3 app.py &
